2eaea2a30e4c91d7926f076c9326913ad24ca9cf
preddy <pradeep.reddy@avotus.com>
2022-11-30 19:00:32 +0530

    ff

--- springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableMongoRepositories
 public class SpringbootBackendApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
